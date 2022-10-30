a33eae958cef6404a63b10e13042c55159d87eac
rbverissimo <rverissimo.dev@gmail.com>
2022-10-30 13:41:33 -0300

    creating basic import sqlite3 and list structure to work

---
+++ sqlite/main.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+customer_list = [(1998, "Anivaldo Soares", "Plumber"),
+                 (2002, "Gerald Ferraz", "Journalist"),
+                 (2002, "Ana da Silva", "Journalist"),
+                 (2005, "Renato Tavares", "Butcher"),
+                 (1999, "Maria Rogeria", "Economist")
+                 ]
