776d819fa82c4ba830cd6906aa81da64b4f99d1f
rbverissimo <rverissimo.dev@gmail.com>
2022-10-30 13:52:20 -0300

    execute create and insert itens into the db file

--- sqlite/main.py
+++ sqlite/main.py
@@ -1,6 +1,9 @@
 import sqlite3
 
 connection = sqlite3.connect('customers.db')
+cursor = connection.cursor()  # this guy here is responsible to operate the database
+
+cursor.execute('create table customers(year_joined integer, customer_name text, occupation text)')
 
 customer_list = [(1998, "Anivaldo Soares", "Plumber"),
                  (2002, "Gerald Ferraz", "Journalist"),
@@ -8,5 +11,7 @@ customer_list = [(1998, "Anivaldo Soares", "Plumber"),
                  (2005, "Renato Tavares", "Butcher"),
                  (1999, "Maria Rogeria", "Economist")
                  ]
+
+cursor.executemany('insert into customers values(?,?,?)', customer_list)
 
 connection.close()
