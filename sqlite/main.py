2990b8c5885ffb9d421c4d3e9ed998f0d45428d0
rbverissimo <rverissimo.dev@gmail.com>
2022-10-30 13:46:42 -0300

    creating sql3 connection and the db

--- sqlite/main.py
+++ sqlite/main.py
@@ -1,8 +1,12 @@
 import sqlite3
 
+connection = sqlite3.connect('customers.db')
+
 customer_list = [(1998, "Anivaldo Soares", "Plumber"),
                  (2002, "Gerald Ferraz", "Journalist"),
                  (2002, "Ana da Silva", "Journalist"),
                  (2005, "Renato Tavares", "Butcher"),
                  (1999, "Maria Rogeria", "Economist")
                  ]
+
+connection.close()
