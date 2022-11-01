4dd0c64eceffc898952d64d0650852d94e91bd2e
rbverissimo <rverissimo.dev@gmail.com>
2022-11-01 10:59:59 -0300

    fetching from the job_type, prepararing to replace things between tables

--- sqlite/table-operations-read-sqlite3.py
+++ sqlite/table-operations-read-sqlite3.py
@@ -14,5 +14,10 @@ print("\n")
 cursor.execute('select * from customers where occupation=:c', {"c": "Journalist"})
 journalist_search = cursor.fetchall()
 print(journalist_search)
+cursor.execute('select * from job_type where type=:c', {"c": "Communication"})
+job_type_search = cursor.fetchall()
+
+print("\n")
+print(job_type_search)
 
 connection.close()
