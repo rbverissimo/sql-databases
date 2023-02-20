14416080f9ee9a0cfca29192c5013b42361fee09
rbverissimo <rverissimo.dev@gmail.com>
2023-02-20 14:48:31 -0300

    Implementado o método de busca por Id nos inquilinos

--- projeto-imobiliaria/read-inquilinos.py
+++ projeto-imobiliaria/read-inquilinos.py
@@ -25,4 +25,24 @@ def buscarTodosInquilinos():
             sqliteConnection.close()
             print('Conexão fechada')
 
-buscarTodosInquilinos()
+def buscarInquilinosPorId(id):
+    try:
+        sqliteConnection = sqlite3.connect('imobiliaria.db')
+        cursor = sqliteConnection.cursor()
+        print('Conectado ao banco de dados')
+
+        sql_select_id = """select * from inqulinos where id = ?"""
+        cursor.execute(sql_select_id, id)
+        resultado = cursor.fetchall()
+        print('Id buscado:', id)
+
+        for r in resultado:
+            print('Nome', r[1])
+
+        cursor.close()
+    except sqlite3.Error as error:
+        print('Leitura não realizada: ', error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print('Conexão fechada')
