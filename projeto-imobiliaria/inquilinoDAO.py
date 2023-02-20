4fe734efaed3d60ab0f0157bdf4c994f797334f5
rbverissimo <rverissimo.dev@gmail.com>
2023-02-20 15:37:01 -0300

    Implementado um método main para servir de teste dos métodos implementados no arquivo inquilinoDAO

---
+++ projeto-imobiliaria/inquilinoDAO.py
@@ -0,0 +1,92 @@
+import sqlite3
+
+
+def buscarTodosInquilinos():
+    try:
+        sqliteConnection = sqlite3.connect('imobiliaria.db')
+        cursor = sqliteConnection.cursor()
+        print('Conectado ao banco de dados')
+
+        sql_select = """select * from inquilinos"""
+        cursor.execute(sql_select)
+        resultado = cursor.fetchall()
+        print('Total de registros: ', len(resultado))
+
+        for r in resultado:
+            print('Id:', r[0])
+            print('Nome: ', r[1])
+            print('\n')
+
+        cursor.close()
+    except sqlite3.Error as error:
+        print('Leitura não realizada: ', error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print('Conexão fechada')
+
+
+def buscarInquilinosPorId(id):
+    try:
+        sqliteConnection = sqlite3.connect('imobiliaria.db')
+        cursor = sqliteConnection.cursor()
+        print('Conectado ao banco de dados')
+
+        sql_select_id = """select * from inqulinos where id = ?"""
+        cursor.execute(sql_select_id, id)
+        resultado = cursor.fetchall()
+        print('Id buscado:', id)
+
+        for r in resultado:
+            print('Nome', r[1])
+
+        cursor.close()
+    except sqlite3.Error as error:
+        print('Leitura não realizada: ', error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print('Conexão fechada')
+
+
+def inserirNovoInquilino(id, nome):
+    try:
+        sqlite_connection = sqlite3.connect('imobiliaria.db')
+        cursor = sqlite_connection.cursor()
+        print('Conexão com o banco de dados realizada')
+
+        sql = """insert into inquilinos (id, nome) values (?, ?)"""
+        data = (id, nome)
+        cursor.execute(sql, data)
+        sqlite_connection.commit()
+        print('Entrada de dados feita com sucesso')
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print('Falha ao inserir', error)
+
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print('Conexão com banco de dados fechada')
+
+
+def inserirNovosInquilinos(listaDeInquilinos):
+    try:
+        sqlite_connection = sqlite3.connect('imobiliaria.db')
+        cursor = sqlite_connection.cursor()
+        print('Conectado ao banco de dados')
+        sql_query = """insert into inquilinos(id, nome) values(?, ?);"""
+        cursor.executemany(sql_query, listaDeInquilinos)
+        sqlite_connection.commit()
+        print(cursor.rowcount, 'linhas afetadas')
+        cursor.close()
+
+
+    except sqlite3.Error as error:
+        print('Falha ao inserir várias colunas', error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print('Conexão com o banco de dados fechada')
---
+++ projeto-imobiliaria/main.py
@@ -0,0 +1,10 @@
+from inquilinoDAO import inserirNovosInquilinos
+
+
+def main():
+    listaParaInserir = [(3, 'Danila'), (4, 'Ezequiel'), (5, 'Paulinho')]
+    inserirNovosInquilinos(listaParaInserir)
+
+
+if __name__ == "__main__":
+    main()
