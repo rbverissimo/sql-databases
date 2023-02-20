75cccf5c30bb12a257ad634614e55f32dfeee6e9
rbverissimo <rverissimo.dev@gmail.com>
2023-02-20 18:21:38 -0300

    Commit para atualizar o banco de dados

--- projeto-imobiliaria/main.py
+++ projeto-imobiliaria/main.py
@@ -1,7 +1,19 @@
-from anosDAO import inserirNovoAno
+from mesesDAO import inserirNovosMeses
 
 def main():
-    inserirNovoAno(1, 2023)
+    listaDeMeses = [(1, "Janeiro"),
+                    (2, "Fevereiro"),
+                    (3, "Março"),
+                    (4, "Abril"),
+                    (5, "Maio"),
+                    (6, "Junho"),
+                    (7, "Julho"),
+                    (8, "Agosto"),
+                    (9, "Setembro"),
+                    (10, "Outubro"),
+                    (11, "Novembro"),
+                    (12, "Dezembro")]
+    inserirNovosMeses(listaDeMeses)
 
 if __name__ == "__main__":
     main()
