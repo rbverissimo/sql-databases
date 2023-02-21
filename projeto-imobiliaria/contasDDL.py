c68dbc24479c81e0467a33eaf879aa8e3a367dea
rbverissimo <rverissimo.dev@gmail.com>
2023-02-21 13:44:41 -0300

    Atualização do banco de dados. Implementação do método para criação da tabela de contas definindo uma CK composta por ano, mês e o proprietário.

--- projeto-imobiliaria/contasDDL.py
+++ projeto-imobiliaria/contasDDL.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def criarTabelaMes():
+def criarTabelaContas():
     try:
         sqliteConnection = sqlite3.connect('imobiliaria.db')
         cursor = sqliteConnection.cursor()
@@ -8,7 +8,15 @@ def criarTabelaMes():
 
         sql_create = """CREATE TABLE contas(
                     agua integer not null,
-                    luz integer not null)"""
+                    luz integer not null,
+                    ano_id integer,
+                    mes_id integer,
+                    prop_id integer,
+                    FOREIGN KEY(ano_id) REFERENCES anos(id),
+                    FOREIGN KEY(mes_id) REFERENCES meses(id),
+                    FOREIGN KEY(prop_id) REFERENCES propriedades(id),
+                    PRIMARY KEY(ano_id, mes_id, prop_id)
+                    );"""
 
         cursor.execute(sql_create)
         sqliteConnection.commit()
--- projeto-imobiliaria/main.py
+++ projeto-imobiliaria/main.py
@@ -1,19 +1,8 @@
 from mesesDAO import inserirNovosMeses
+from contasDDL import criarTabelaContas
 
 def main():
-    listaDeMeses = [(1, "Janeiro"),
-                    (2, "Fevereiro"),
-                    (3, "Março"),
-                    (4, "Abril"),
-                    (5, "Maio"),
-                    (6, "Junho"),
-                    (7, "Julho"),
-                    (8, "Agosto"),
-                    (9, "Setembro"),
-                    (10, "Outubro"),
-                    (11, "Novembro"),
-                    (12, "Dezembro")]
-    inserirNovosMeses(listaDeMeses)
+    criarTabelaContas()
 
 if __name__ == "__main__":
     main()
