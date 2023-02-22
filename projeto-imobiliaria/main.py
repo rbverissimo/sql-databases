d1bbffabb2041268d3de1d268a0e5e27dcdf232f
rbverissimo <rverissimo.dev@gmail.com>
2023-02-22 14:40:02 -0300

    Snapshot da criação da nova tabela de contas com a modelagem referida no commit anterior acertada

--- projeto-imobiliaria/main.py
+++ projeto-imobiliaria/main.py
@@ -1,8 +1,13 @@
-from mesesDAO import inserirNovosMeses
-from contasDDL import criarTabelaContas
+from contasDDL import *
+from anoDDL import *
 
 def main():
+    dropTabelaContas()
+    dropTabelaAnos()
+    criarTabelaAno()
     criarTabelaContas()
+
+    
 
 if __name__ == "__main__":
     main()
